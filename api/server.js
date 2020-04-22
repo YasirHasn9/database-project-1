18014b912ee4600c0c5f73787c5c9e6d1c24238f
YasirHasn9 <hassanyasir223@gmail.com>
2020-04-22 01:47:11 -0700

    get accounts

--- api/server.js
+++ api/server.js
@@ -1,9 +1,23 @@
 const express = require("express");
-
 const db = require("../data/dbConfig.js");
 
 const server = express();
 
 server.use(express.json());
 
+server.get("/", (req, res) => {
+  res.json({
+    message: "Hello"
+  });
+});
+
+server.get("/accounts", async (req, res, next) => {
+  try {
+    const accounts = await db.select("*").from("accounts");
+    res.json(accounts);
+  } catch (err) {
+    console.log("get", err);
+    next(next);
+  }
+});
 module.exports = server;
--- index.js
+++ index.js
@@ -2,6 +2,14 @@ const server = require("./api/server.js");
 
 const PORT = process.env.PORT || 5000;
 
+server.use((err, req, res, next) => {
+  console.error(err);
+
+  res
+    .status(500)
+    .json({ message: "There was an error performing the required operation" });
+});
+
 server.listen(PORT, () => {
   console.log(`\n== API running on port ${PORT} ==\n`);
 });
